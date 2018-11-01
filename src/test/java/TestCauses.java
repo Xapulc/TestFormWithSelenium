6848d0ffad8a20445a4eb39cb3dca13291c1bc8d
Xapulc <xarik1999@gmail.com>
2018-11-01 19:41:22 +0300

    add locators

--- src/test/java/TestCauses.java
+++ src/test/java/TestCauses.java
@@ -6,53 +6,65 @@ public class TestCauses extends BaseRunner {
     @Test
     public void TestClickWithFill() {
         driver.get(baseUrl);
-        driver.findElement(By.name("fio")).click();
-        driver.findElement(By.name("fio")).clear();
-        driver.findElement(By.name("fio"))
-                .sendKeys("Иванов");
+        WebElement fio = driver.findElement(By.cssSelector("[name='fio']"));
+        WebElement email = driver.findElement(By.cssSelector("[name='email']"));
+        WebElement phone = driver.findElement(By.cssSelector("[name='phone']"));
+        WebElement city = driver.findElement(By.cssSelector("[name='city']"));
 
-        driver.findElement(By.name("email")).clear();
-        driver.findElement(By.name("email"))
-                .sendKeys("testing@test.");
+        fio.click();
+        fio.clear();
+        fio.sendKeys("Иванов");
 
-        driver.findElement(By.name("phone")).click();
-        driver.findElement(By.name("phone")).clear();
-        driver.findElement(By.name("phone"))
-                .sendKeys("+7 (000) 000-00-00");
+        email.clear();
+        email.sendKeys("testing@test.");
 
-        driver.findElement(By.name("city")).click();
-        driver.findElement(By.name("city")).clear();
-        driver.findElement(By.name("city"))
-                .sendKeys("Москва-1");
+        phone.click();
+        phone.clear();
+        phone.sendKeys("+7 (000) 000-00-00");
+
+        city.click();
+        city.clear();
+        city.sendKeys("Москва-1");
         clickFreeSpace();
 
         assertEquals("Недостаточно информации. Введите фамилию, имя и отчество через пробел (Например: Иванов Иван Алексеевич)",
-                driver.findElement(By.cssSelector("div.Error__errorMessage_q8BBY")).getText());
+                driver.findElement(By.cssSelector(
+                        "#form div ~ div.Error__errorMessage_q8BBY")).getText());
         assertEquals("Введите корректный адрес эл. почты",
-                driver.findElement(By.cssSelector(".Row__row_AjrJL:nth-child(2) .Error__errorMessage_q8BBY")).getText());
+                driver.findElement(By.cssSelector("#form div.Row__row_AjrJL:nth-child(2) div.Error__errorMessage_q8BBY")).getText());
         assertEquals("Код города/оператора должен начинаться с цифры 3, 4, 5, 6, 8, 9",
-                driver.findElement(By.cssSelector(".FormField__field_1iwkM:nth-child(2) > .Error__errorMessage_q8BBY")).getText());
+                driver.findElement(By.cssSelector("#form div.Row__row_AjrJL:nth-child(2) div[class*=FormField]:nth-child(2) div[class*=Error]")).getText());
         assertEquals("Допустимо использовать только буквы русского, латинского алфавита и дефис",
-                driver.findElement(By.cssSelector(".Row__row_AjrJL:nth-child(3) .Error__errorMessage_q8BBY")).getText());
+                driver.findElement(By.cssSelector("#form div.Row__row_AjrJL:nth-child(3) div.Error__errorMessage_q8BBY")).getText());
     }
 
     @Test
     public void TestClickWithoutFill() {
         driver.get(baseUrl);
-        driver.findElement(By.name("fio")).click();
-        driver.findElement(By.name("email")).click();
-        driver.findElement(By.name("phone")).click();
-        driver.findElement(By.name("city")).click();
+        WebElement fio = driver.findElement(By.xpath("//form//*[@name='fio']"));
+        WebElement email = driver.findElement(By.xpath("//form//*[@name='email']"));
+        WebElement phone = driver.findElement(By.xpath("//form//*[@name='phone']"));
+        WebElement city = driver.findElement(By.xpath("//form//*[@name='city']"));
+
+        fio.click();
+        email.click();
+        phone.click();
+        city.click();
+
         clickFreeSpace();
 
-        assertEquals("Поле обязательное", driver.findElement(By.cssSelector(
-                "div.Error__errorMessage_q8BBY")).getText());
-        assertEquals("Поле обязательное", driver.findElement(By.cssSelector(
-                "div.Error__errorMessage_q8BBY")).getText());
-        assertEquals("Поле обязательное", driver.findElement(By.cssSelector(
-                ".Row__row_AjrJL:nth-child(2) .Error__errorMessage_q8BBY")).getText());
-        assertEquals("Поле обязательное", driver.findElement(By.cssSelector(
-                ".Row__row_AjrJL:nth-child(3) .Error__errorMessage_q8BBY")).getText());
+        assertEquals("Поле обязательное", driver.findElement(By.xpath(
+                "//form//div[@data-qa='fio']/../following-sibling::"
+                        + "div[@class='Error__errorMessage_q8BBY']")).getText());
+        assertEquals("Поле обязательное", driver.findElement(By.xpath(
+                "//form//div[@data-qa='email']/following-sibling::"
+                        + "div[@class='Error__errorMessage_q8BBY']")).getText());
+        assertEquals("Необходимо указать номер телефона", driver.findElement(By.xpath(
+                "//form//*[@name='phone']/../../../following-sibling::"
+                        + "div[@class='Error__errorMessage_q8BBY']")).getText());
+        assertEquals("Поле обязательное", driver.findElement(By.xpath(
+                "//form//*[@name='city']/../../../following-sibling::"
+                        + "div[@class='Error__errorMessage_q8BBY']")).getText());
     }
 
     private void clickFreeSpace() {
